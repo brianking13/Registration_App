c0348bb0d586b27007e48f490d9609f9a9d5cec9
brianking13 <bkingrr13@gmail.com>
2020-04-10 10:51:25 -0700

    Inital File Commit

---
+++ api.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import pandas as  pd
+
+
+def initial_setup():
+    file = Path("data.xlsx")
+    if file.exists():
+        return True
+    else:
+        data = pd.DataFrame({'LAST NAME':[],'FIRST NAME':[],'ID':[],'Comments':[]})
+        writer = pd.ExcelWriter('data.xlsx')
+        data.to_excel(writer,'Data', index = False)
+        writer.save()
---
+++ main.py
@@ -0,0 +1,12 @@
+import api
+
+def main():
+    api.initial_setup()
+
+
+
+
+
+    
+if __name__ == '__main__':
+    main()
